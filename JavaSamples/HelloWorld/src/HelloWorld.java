357f90859fe17f87f3b574ac7cbb87ca91d6459e
pva <pva@debian10>
2021-01-07 09:20:26 +0100

    First Version

---
+++ JavaSamples/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,21 @@
+
+
+// Ceci est une classe
+public class HelloWorld {
+
+	// Ceci est une méthode statique.
+	public static void main(String[] args) {
+		// out est une PrintStream
+		// out est un attribut statique de la classe System
+		// Une classe commence par une Majuscule
+		// Un attribut commence par une Minuscule
+		System.out.println("Hello world !!!");
+		System.out.println("Second line");
+		System.out.print("line");
+		System.out.print("line");
+		System.out.print("line");
+		System.out.print("line");
+		System.out.print("\nline");
+
+	}
+}
